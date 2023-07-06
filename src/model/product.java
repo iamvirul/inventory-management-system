c1832491ed80fb7ae94b0c70bf354f807783d768
Virul Nirmala <virulnirmala24@gmail.com>
2023-07-06 16:38:41 +0530

    full project uploaded

---
+++ src/model/product.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author virulnirmala
+ */
+public class product {
+      private String id;
+    private String brand;
+    private String modal;
+    private String price;
+    private String qty;
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * @return the modal
+     */
+    public String getModal() {
+        return modal;
+    }
+
+    /**
+     * @param modal the modal to set
+     */
+    public void setModal(String modal) {
+        this.modal = modal;
+    }
+
+    /**
+     * @return the price
+     */
+    public String getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the qty
+     */
+    public String getQty() {
+        return qty;
+    }
+
+    /**
+     * @param qty the qty to set
+     */
+    public void setQty(String qty) {
+        this.qty = qty;
+    }
+    
+}
